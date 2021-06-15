0c092e522c088860279011a87e622b42b8485ec8
Mr-DOY <83167364+Mr-DOY@users.noreply.github.com>
2021-06-15 23:36:13 +0700

    Add files via upload

---
+++ dah.sh
@@ -0,0 +1,48 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[37;1m'
+sleep 1
+echo
+toilet -f big -F gay Mr.DOY108/.
+echo $green"Selamat Datang kawan"
+echo
+sleep 2
+echo $red"Silahkan Pilih Tools Yang Disediakan Oleh Kami"
+echo
+echo $cyan"1.) Stabilkan Jaringan"
+echo
+echo $blue"0.) Keluar aja dech !!!"
+echo
+echo $white
+read -p "PilihMana : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Mr.DOY108/.
+echo $green" Stabilkan jaringan by Mr.DOY108/."
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $red
+figlet "bye"
+sleep 2
+echo $red"salam gw ke kalian"
+sleep 2
+echo $green
+figlet "Mr.DOY108/."
+echo
+echo $white"To Be Continued"
+sleep 3
+exit
+fi
+
